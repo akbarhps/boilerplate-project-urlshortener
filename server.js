7646501d7564692bf2cb29d464bba4bbcd59d1e1
akbarhps <akbarputrasrg@gmail.com>
2021-04-10 17:58:20 +0700

    url shortener

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+const Url = require('./models/url.model');
 const app = express();
 
 // Basic Configuration
@@ -8,17 +10,55 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+mongoose.connect(process.env.DB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("Connected to Database"));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl/new', async function (req, res) {
+    const url = new Url();
+    const originalUrl = req.body.original_url;
+    try {
+        const isValid = new URL(originalUrl);
+        url.original_url = originalUrl;
+        url.short_url = randomString(7);
+        await url.save();
+        res.json(url);
+    } catch (e) {
+        res.send({error: 'invalid url'});
+    }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shorturl/:short_url', async (req, res) => {
+    const short_url = req.params.short_url;
+    try {
+        const result = await Url.findOne({short_url});
+        if (!result) {
+            res.json({error: "invalid url"});
+        }
+        res.redirect(result.original_url);
+    } catch (e) {
+        res.json({error: e.message});
+    }
 });
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+});
+
+const randomString = (n) => {
+    return [...Array(n)].map(i => (~~(Math.random() * 36)).toString(36)).join('')
+}
